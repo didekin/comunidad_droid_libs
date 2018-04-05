7ee972edb1ecaa6c1b5319806b08c5b97f0627a6
Pedro Nevado <pedro@didekin.es>
2018-04-05 10:11:33 +0200

    version a-0.1.4

--- lib_one/src/debug/java/com/didekindroid/lib_one/testutil/ViewerSavedStateWrapper.java
+++ lib_one/src/debug/java/com/didekindroid/lib_one/testutil/ViewerSavedStateWrapper.java
@@ -1,27 +1,19 @@
 package com.didekindroid.lib_one.testutil;
 
-import android.os.Bundle;
-
-import com.didekindroid.lib_one.api.Viewer;
-
 import java.util.concurrent.atomic.AtomicReference;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static com.didekindroid.lib_one.testutil.ConstantForMethodCtrlExec.BEFORE_METHOD_EXEC;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.waitAtMost;
-import static org.hamcrest.CoreMatchers.is;
 
 /**
  * User: pedro@didekin
  * Date: 16/09/17
  * Time: 15:00
  */
-@SuppressWarnings("unused")
-public class ViewerTestWrapper {
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class ViewerSavedStateWrapper {
 
-    private final String AFTER_SaveState = "afterSaveStateMethod";
-    private final AtomicReference<String> flagMethod = new AtomicReference<>(BEFORE_METHOD_EXEC);
+    public final String AFTER_SaveState = "afterSaveStateMethod";
+    public final AtomicReference<String> flagMethod = new AtomicReference<>(BEFORE_METHOD_EXEC);
 
     public int clearSubscriptions()
     {
@@ -34,9 +26,9 @@ public class ViewerTestWrapper {
         flagMethod.set(AFTER_SaveState);
     }
 
-    public void checkOnSaveInstanceState(final Viewer viewer)
+    /*public void checkOnSaveInstanceState(final Viewer viewer)
     {
         viewer.getActivity().runOnUiThread(() -> getInstrumentation().callActivityOnSaveInstanceState(viewer.getActivity(), new Bundle(0)));
         waitAtMost(6, SECONDS).untilAtomic(flagMethod, is(AFTER_SaveState));
-    }
+    }*/
 }
--- lib_one_test/src/main/java/com/didekindroid/lib_one/testutil/MyClass.java
+++
@@ -1,4 +0,0 @@
-package com.didekindroid.lib_one.testutil;
-
-public class MyClass {
-}
