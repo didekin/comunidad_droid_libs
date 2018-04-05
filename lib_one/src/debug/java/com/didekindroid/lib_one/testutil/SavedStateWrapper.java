cd55b2a824f0643dfaaee5d8a025c26ce58fa498
Pedro Nevado <pedro@didekin.es>
2018-04-05 11:17:43 +0200

    version a-0.1.8

--- lib_one/src/debug/java/com/didekindroid/lib_one/testutil/SavedStateWrapper.java
+++ lib_one/src/debug/java/com/didekindroid/lib_one/testutil/SavedStateWrapper.java
@@ -13,13 +13,7 @@ import static com.didekindroid.lib_one.testutil.ConstantForMethodCtrlExec.BEFORE
 public class SavedStateWrapper {
 
     public static final String AFTER_SaveState = "afterSaveStateMethod";
-    public static final String AFTER_ClearSubscriptions = "afterClearSubscriptions";
     private final AtomicReference<String> flagMethod = new AtomicReference<>(BEFORE_METHOD_EXEC);
-
-    public void clearSubscription()
-    {
-        flagMethod.set(AFTER_ClearSubscriptions);
-    }
 
     public void saveState()
     {
