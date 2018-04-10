d253aeb8857faecb4630928943fdc40dd9a78033
Pedro Nevado <pedro@didekin.es>
2018-04-10 20:33:02 +0200

    version a-0.1.18

--- lib_one/src/androidTest/java/com/didekindroid/lib_one/usuario/router/UserUiExceptionActionTest.java
+++ lib_one/src/androidTest/java/com/didekindroid/lib_one/usuario/router/UserUiExceptionActionTest.java
@@ -11,6 +11,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.intent.Intents.intended;
@@ -19,9 +21,16 @@ import static com.didekindroid.lib_one.RouterInitializer.routerInitializer;
 import static com.didekindroid.lib_one.testutil.EspressoTestUtil.isResourceIdDisplayed;
 import static com.didekindroid.lib_one.testutil.EspressoTestUtil.isToastInView;
 import static com.didekindroid.lib_one.testutil.InitializerTestUtil.initSec_Http_Router;
+import static com.didekindroid.lib_one.usuario.UserTestData.CleanUserEnum.CLEAN_RODRIGO;
+import static com.didekindroid.lib_one.usuario.UserTestData.cleanOptions;
+import static com.didekindroid.lib_one.usuario.UserTestData.comu_real_rodrigo;
+import static com.didekindroid.lib_one.usuario.UserTestData.regUserComuWithTkCache;
 import static com.didekindroid.lib_one.usuario.UserTestNavigation.loginAcResourceId;
+import static com.didekindroid.lib_one.usuario.UserTestNavigation.userDataAcRsId;
 import static com.didekindroid.lib_one.usuario.router.UserUiExceptionAction.show_login_noUser;
 import static com.didekindroid.lib_one.usuario.router.UserUiExceptionAction.show_login_tokenNull;
+import static com.didekindroid.lib_one.usuario.router.UserUiExceptionAction.show_userData_wrongMail;
+import static com.didekinlib.http.usuario.UsuarioExceptionMsg.PASSWORD_NOT_SENT;
 import static com.didekinlib.http.usuario.UsuarioExceptionMsg.TOKEN_NULL;
 import static com.didekinlib.http.usuario.UsuarioExceptionMsg.USERCOMU_WRONG_INIT;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -70,7 +79,16 @@ public class UserUiExceptionActionTest {
         run(ue, show_login_tokenNull, loginAcResourceId);
     }
 
-    // TODO: falta test de un action.
+    @Test
+    public void test_show_userData_wrongMail() throws IOException, UiException
+    {
+        // Preconditions.
+        regUserComuWithTkCache(comu_real_rodrigo);
+        final UiException ue = new UiException(new ErrorBean(PASSWORD_NOT_SENT));
+        run(ue, show_userData_wrongMail, userDataAcRsId);
+
+        cleanOptions(CLEAN_RODRIGO);
+    }
 
     // ============================  Helpers ==============================
 
