678553b546e8a704527a934d0e8d2ea4f48a03e3
Pedro Nevado <pedro@didekin.es>
2018-08-21 13:56:16 +0200

    version b.1.5

---
+++ lib_one/src/main/java/com/didekindroid/lib_one/util/RxJavaUtil.java
@@ -0,0 +1,37 @@
+package com.didekindroid.lib_one.util;
+
+import android.support.annotation.NonNull;
+
+import com.didekindroid.lib_one.api.exception.UiException;
+
+import java.io.Serializable;
+
+import io.reactivex.Maybe;
+import io.reactivex.MaybeSource;
+import io.reactivex.functions.Function;
+import retrofit2.Response;
+
+import static com.didekindroid.lib_one.HttpInitializer.httpInitializer;
+import static io.reactivex.Maybe.empty;
+
+public class RxJavaUtil {
+
+    private RxJavaUtil()
+    {
+    }
+
+    @NonNull
+    private static <T extends Serializable> Function<Response<T>, MaybeSource<T>> getResponseMaybeSourceFunction()
+    {
+        return response -> {
+            if (response.body() == null) {
+                if (response.errorBody() == null){
+                    return empty();
+                }else {
+                    return Maybe.error(new UiException(httpInitializer.get().getHttpHandler().getErrorBean(response)));
+                }
+
+            } else return Maybe.just(httpInitializer.get().getResponseBody(response));
+        };
+    }
+}
